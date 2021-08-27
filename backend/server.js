64758e6021ab11e8d0d609fd0352433b7bbf6b3c
Victor Foryi <foryi@v.rada.gov.ua>
2021-08-27 14:47:03 +0300

    Backend Authentication

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import {notFound, errorHandler} from "./middleware/errorMiddleware.js"
 import connectDB from "./config/db.js"
 
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config()
 
@@ -12,11 +13,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
@@ -27,5 +31,5 @@ app.listen(
     PORT,
     console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-)
+    )
 )
