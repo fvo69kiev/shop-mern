550620ec5cafc09c99162826d2940e63d8f1e3c6
Victor Foryi <foryi@v.rada.gov.ua>
2021-09-16 14:27:36 +0300

    Admin -2

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -64,13 +64,32 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 
     if (order) {
         order.isPaid = true
-        order.paiAt = Date.now()
+        order.paidAt = Date.now()
         order.paymentResult = {
             id: req.body.id,
             status: req.body.status,
             update_time: req.body.update_time,
             email_address: req.body.payer.email_address
         }
+
+        const updateOrder = await order.save()
+
+        res.json(updateOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+// @desc        Update order to delivered
+// @route       GET /api/orders/:id/deliver
+// @access      Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isDelivered = true
+        order.deliveredAt = Date.now()
 
         const updateOrder = await order.save()
 
@@ -89,4 +108,19 @@ const getMyOrders = asyncHandler(async (req, res) => {
     res.json(orders)
 })
 
-export {addOrderItems, getOrderById, updateOrderToPaid, getMyOrders}
+// @desc        Get all orders
+// @route       GET /api/orders
+// @access      Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find({}).populate('user', 'id name')
+    res.json(orders)
+})
+
+export {
+    addOrderItems,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getMyOrders,
+    getOrders,
+}
